b227f08049bd60dc80606fc9fa3b59c0c873ba7f
Leslie Baker <leslie.baker212@gmail.com>
2015-02-25 17:20:29 -0700

    Login touch-ups and fix rest macro to not make some calls when not logged in

--- src/rest.clj
+++ src/rest.clj
@@ -1,17 +1,26 @@
 (ns rest)
 
-(defmacro endpoint [collection url element-id]
-  (let [endpoint-symbol (symbol (str collection "-endpoint"))
-        db-symbol       (symbol (str collection "-db"))]
-    `(do
-       (def ~collection (tailrecursion.javelin/cell []))
-       (def ~endpoint-symbol
-         (endpoint* ~url ~element-id ~collection))
-       (def ~db-symbol
-         (tailrecursion.javelin/cell=
-           (datascript/db-with
-             (datascript/empty-db)
-             (map #(assoc % :db/id (~element-id %))
-                  (map #(into {} (remove (comp nil? second) %))
-                       ~collection)))))
-       (~endpoint-symbol :read nil nil))))
+(defmacro endpoints [& endpoints]
+  `(do
+     ~@(map (fn [[collection url element-id requires-token]]
+              (let [endpoint-symbol (symbol (str collection "-endpoint"))
+                    db-symbol       (symbol (str collection "-db"))]
+                `(do
+                   (def ~collection (tailrecursion.javelin/cell []))
+                   (def ~endpoint-symbol
+                     (endpoint ~url ~element-id ~collection ~requires-token))
+                   (def ~db-symbol
+                     (tailrecursion.javelin/cell=
+                       (datascript/db-with
+                         (datascript/empty-db)
+                         (map #(assoc % :db/id (~element-id %))
+                              (map #(into {} (remove (comp nil? second) %))
+                                   ~collection))))))))
+            endpoints)
+     (defn ~'refresh []
+       ~@(map (fn [[collection _ _ requires-token]]
+                `(if (or (not ~requires-token) @app/token)
+                   (~(symbol (str collection "-endpoint")) :read nil nil)
+                   (reset! (symbol collection) [])))
+              endpoints))
+     (~'refresh)))
