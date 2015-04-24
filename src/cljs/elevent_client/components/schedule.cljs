c25ae01ef67e1f1913acc6fbfc6f0098a26f0178
Oscar Marshall <oscar.lim.marshall@gmail.com>
2015-04-24 01:40:58 -0600

    Highlight conflicts

--- src/cljs/elevent_client/components/schedule.cljs
+++ src/cljs/elevent_client/components/schedule.cljs
@@ -1,30 +1,49 @@
 (ns elevent-client.components.schedule
   (:require [cljs-time.coerce :refer [from-string]]
             [cljs-time.format :refer [unparse]]
+            [cljs-time.core :refer [after?]]
             [datascript :as d]
 
             [elevent-client.components.help-icon :as help-icon]
             [elevent-client.locale :as locale]
             [elevent-client.api :as api]))
 
-(defn component
-  [scheduled-activities & [button-text button-action footer-button disabled-condition]]
-  [:table.ui.table
-   [:thead
-    [:tr
-     [:th "Start"]
-     [:th "End"]
-     [:th "Activity"]
-     [:th "Location"]
-     [:th]]]
-   [:tbody
-    (doall
-      (for [[schedule-id activity-id] scheduled-activities]
-        (let [activity
-              (when activity-id
-                (d/entity @api/activities-db activity-id))]
-          ^{:key schedule-id}
-          [:tr
+(defn component [scheduled-activities
+                 & [button-text button-action footer-button disabled-condition]]
+  (let [scheduled-activities
+        (->> scheduled-activities
+             (map (fn [[schedule-id activity-id]]
+                    (assoc (into {} (d/entity @api/activities-db activity-id))
+                      :ScheduleId schedule-id)))
+             (sort-by :StartTime))
+        scheduled-activities
+        (loop [out (into [] scheduled-activities) i 0]
+          (cond
+            (>= (inc i) (count scheduled-activities))
+            out
+
+            (after? (from-string (:EndTime (nth out i)))
+                    (from-string (:StartTime (nth out (inc i)))))
+            (recur (-> out
+                       (assoc-in [i :Conflict] true)
+                       (assoc-in [(inc i) :Conflict] true))
+                   (inc i))
+
+            :else
+            (recur out (inc i))))]
+    [:table.ui.table
+     [:thead
+      [:tr
+       [:th "Start"]
+       [:th "End"]
+       [:th "Activity"]
+       [:th "Location"]
+       [:th]]]
+     [:tbody
+      (doall
+        (for [activity scheduled-activities]
+          ^{:key (:ScheduleId activity)}
+          [:tr {:class (when (:Conflict activity) "error")}
            [:td {:noWrap true}
             (when activity
               [:p
@@ -46,16 +65,22 @@
            [:td.right.aligned {:noWrap true}
             (when button-text
               [:div
-               (when (and disabled-condition (disabled-condition activity-id))
+               (when (and disabled-condition
+                          (disabled-condition (:ActivityId activity)))
                  [help-icon/component "This is a required activity"])
                [:div.ui.small.button
-                {:class (when (and disabled-condition (disabled-condition activity-id)) "disabled")
-                 :on-click #(button-action schedule-id activity-id)}
-                button-text]])]])))]
-   (when footer-button
-     [:tfoot
-      [:tr
-       [:th {:colSpan "6"}
-        footer-button
-        #_[:div.ui.small.labeled.icon.button
-         [:i.print.icon] "Print"]]]])])
+                {:class (when (and disabled-condition
+                                   (disabled-condition (:ActivityId activity)))
+                          "disabled")
+                 :on-click #(button-action (:ScheduleId activity)
+                                           (:ActivityId activity))}
+                (when (:Conflict activity)
+                  [:span [:span.ui.red.label "CONFLICT"] " "])
+                button-text]])]]))]
+     (when footer-button
+       [:tfoot
+        [:tr
+         [:th {:colSpan "6"}
+          footer-button
+          #_[:div.ui.small.labeled.icon.button
+             [:i.print.icon] "Print"]]]])]))
